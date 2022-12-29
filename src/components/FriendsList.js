f63c9e6c22c6923e95b1c0d1e8da228c6f679f06
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-29 10:32:28 -0700

    create FriendsList component functionality

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import '../styles/Login.css'
 
 const FriendsList = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:9000/api/friends', {
+            headers: {
+                authorization: token
+            }
+        })
+            .then(res => {
+                setFriends(res.data);
+                // console.log(res.data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [])
     return (
         <>
         <h3>FRIENDS LIST</h3>
-        
+        <ul>
+        {friends.map(friend => {
+            return(
+                <li key={friend.id}>{`${friend.name.toUpperCase()}    -    ${friend.email.toUpperCase()}`}</li>
+            )
+        })}
+        </ul>
         </>
     )
 }
